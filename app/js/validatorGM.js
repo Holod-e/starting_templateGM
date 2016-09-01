0c30d42ca90e42f3ac094db38563f13a7a4b2a5c
Evhenii Holodniak <holodniak.e@gmail.com>
2016-09-01 15:27:50 +0300

    validation name can be 2 symbols

--- app/js/validatorGM.js
+++ app/js/validatorGM.js
@@ -12,13 +12,13 @@
                  case 'name':
                     // var rv_name = /^[а-яА-ЯёЁa-zA-Z0-9]+$/; // используем регулярное выражение
 
-                    if(val.length > 2 && val != '' )
-                    { 
+                    if(val.length > 1 && val != '' )
+                    {
                        $(this).removeClass('error');
-                      $(this).addClass('not_error'); 
+                      $(this).addClass('not_error');
                     }
                     else
-                    { 
+                    {
                       $(this).removeClass('not_error').addClass('error');
                     }
                 break;
@@ -84,8 +84,8 @@
          // Устанавливаем обработчик потери фокуса для всех полей ввода текста
            $('input.name, input.email, input.phone1, input.phone2, input.phone3').unbind().blur(validateForm); // end blur()
            $('input.name, input.email, input.phone1, input.phone2, input.phone3').keypress(validateForm); // end blur()
-          
-        
+
+
       // Теперь отправим наше письмо
-      
+
       }); // end script
